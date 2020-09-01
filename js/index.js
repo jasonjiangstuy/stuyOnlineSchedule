4ab814a68aeccfddc5627fe3b10935ad5e28af81
jasonjiangstuy <jasonjiangny@gmail.com>
2020-09-01 12:16:41 -0400

    MVP for working schedule

--- js/index.js
+++ js/index.js
@@ -9,19 +9,19 @@ $(function(){
     endOfSchool = 14 * 3600
     endOfDay = 24 * 3600
     passing = 10 * 60
-    function ClientSeconds(){
-        var d = new Date();
-        return (d.getHours() * 3600 + d.getMinutes() * 60 + d.getSeconds());	
+    function ClientSeconds(h, m, s){
+        
+        return (h * 3600 + m * 60 + s);	
     }   
 
     
     // setInterval(function (){console.log('test');}, 1000);
     setInterval(function (){Tick();}, 1000);
 
-    function PeriodLeft(){
+    function PeriodLeft(sec){
         // returns what pd, time of period left(including passing), time of period passed
         // in sec
-        let sec = ClientSeconds()
+
         if (startOfSchool > sec){
             // before school
             return ['b', startOfSchool - sec, sec];
@@ -43,23 +43,42 @@ $(function(){
     // [2, 2970, 2430]
     function Tick(){
         console.log('test');
-        
-        let periodInfo = PeriodLeft();
+        var d = new Date();
+        let h = d.getHours()
+        let m = d.getMinutes()
+        let s = d.getSeconds()
+        let periodInfo = PeriodLeft(ClientSeconds(h, m, s));
         console.log(periodInfo);
-        let 
-        let thisPass = 0;
-        let thisLeft = 0;
+        let thisPd = periodInfo[0];
+        let thisPass = Math.floor(periodInfo[1] / 60);
+        let thisLeft = Math.ceil(periodInfo[2] / 60);
         
         if (periodInfo[2] < 10 * 60){
-            if (periodInfo[2] > -10){
-                thisPd = "Before period"
-                thisLeft = 10;
-            };
-        };
-        thisPd = ''+ toString(periodInfo[0])
+            // if 10 mins left -> start warning countdown
+
+                thisPd = "Before period " + (thisPd + 1).toString();
+                thisPass -= 40;
+                thisLeft += 10;
+                
+
+        }else{
+            thisPd = thisPd.toString();
+        }
+
+
         $('#period').text(thisPd);
-        $('#pass').text(Math.floor(periodInfo[1] / 60))
-        $('#left').text(thisLeft + Math.floor(periodInfo[2] / 60));
+        $('#pass').text(thisPass)
+        $('#left').text(thisLeft);
+
+        function add0(v){
+            if (v < 10){
+                return '0'+ v.toString();
+            }
+            else{
+                return v.toString()
+            }
+        }
+        $('#time').text(h.toString() + ':' + add0(m) + ':' + add0(s))
     }
 
     
